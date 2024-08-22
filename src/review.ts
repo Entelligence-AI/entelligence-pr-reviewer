0d2bfb116f59c2f0ced99fd17154f5789893e12e
jason3067812 <jason3067812@gmail.com>
2024-08-21 19:44:59 -0700

    migrate model to gpt4o series

--- src/review.ts
+++ src/review.ts
@@ -63,7 +63,7 @@ export const codeReview = async (
     return
   }
 
-  // as gpt-3.5-turbo isn't paying attention to system message, add to inputs for now
+  // as gpt-4o-mini isn't paying attention to system message, add to inputs for now
   inputs.systemMessage = options.systemMessage
 
   // get SUMMARIZE_TAG message
